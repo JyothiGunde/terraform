7d0b72efbf2be1fc41c1edcfd16ad30d80cc4e99
JyothiGunde <163575401+JyothiGunde@users.noreply.github.com>
2025-06-02 17:58:43 +0530

    Add files via upload

---
+++ variables/variable.tf
@@ -0,0 +1,20 @@
+
+variable "os" {
+    default = "ami-0953476d60561c955"
+}
+
+variable "size" {
+    default = "t2.micro"
+}
+
+variable "name" {
+    default = "Helloworld"
+}
+
+variable "bucketname" {
+      
+}
+
+variable "username" {
+  
+}
